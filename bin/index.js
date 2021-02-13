2cb7447da40253e87dd80cd4c4cf5c6c627a0a96
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-02-13 17:22:58 +0100

    delete component implementation 🚩

--- bin/index.js
+++ bin/index.js
@@ -90,8 +90,7 @@ yargs
         .positional("-r", {
           alias: "--redux",
           type: "string",
-          describe:
-            "to create basic redux implementation structure (it does not require <FileName> argument)",
+          describe: "to create basic redux implementation structure",
         });
     },
     function (argv) {
@@ -100,8 +99,11 @@ yargs
           `app/components/${argv.component}.js`,
           componentTemplate(argv.component),
           function (err) {
-            if (err) throw err;
-            console.log(`app/components/${argv.component}.js created`);
+            if (err) {
+              console.log(`Unable to create ${argv.component} component`);
+            } else {
+              console.log(`app/components/${argv.component}.js created`);
+            }
           }
         );
       } else if (argv.screen) {
@@ -109,20 +111,26 @@ yargs
           `app/screens/${argv.screen}/ui/${argv.screen}UI.js`,
           screenUITemplate(argv.screen),
           function (err) {
-            if (err) throw err;
-            console.log(
-              `app/screens/${argv.screen}/ui/${argv.screen}UI.js created`
-            );
+            if (err) {
+              console.log(`Unable to create ${argv.screen} screen UI`);
+            } else {
+              console.log(
+                `app/screens/${argv.screen}/ui/${argv.screen}UI.js created`
+              );
+            }
           }
         );
         fs.writeFile(
           `app/screens/${argv.screen}/functions/index.js`,
           screenFunctionsTemplate(argv.screen),
           function (err) {
-            if (err) throw err;
-            console.log(
-              `app/screens/${argv.screen}/functions/index.js created`
-            );
+            if (err) {
+              console.log(`Unable to create ${argv.screen} screen functions`);
+            } else {
+              console.log(
+                `app/screens/${argv.screen}/functions/index.js created`
+              );
+            }
           }
         );
       } else if (argv.redux) {
@@ -130,36 +138,83 @@ yargs
           `app/${argv.redux}/actions/index.js`,
           actionsTemplate(argv.redux),
           function (err) {
-            if (err) throw err;
-            console.log(`app/${argv.redux}/actions/index.js created`);
+            if (err) {
+              console.log(`Unable to create ${argv.redux} redux actions`);
+            } else {
+              console.log(`app/${argv.redux}/actions/index.js created`);
+            }
           }
         );
         fs.writeFile(
           `app/${argv.redux}/constants/index.js`,
           constantsTemplate(argv.redux),
           function (err) {
-            if (err) throw err;
-            console.log(`app/${argv.redux}/constants/index.js created`);
+            if (err) {
+              console.log(`Unable to create ${argv.redux} redux constants`);
+            } else {
+              console.log(`app/${argv.redux}/constants/index.js created`);
+            }
           }
         );
         fs.writeFile(
           `app/${argv.redux}/reducers/index.js`,
           reducersTemplate(argv.redux),
           function (err) {
-            if (err) throw err;
-            console.log(`app/${argv.redux}/reducers/index.js created`);
+            if (err) {
+              console.log(`Unable to create ${argv.redux} redux reducers`);
+            } else {
+              console.log(`app/${argv.redux}/reducers/index.js created`);
+            }
           }
         );
         fs.writeFile(
           `app/${argv.redux}/store/index.js`,
           storeTemplate(argv.redux),
           function (err) {
-            if (err) throw err;
-            console.log(`app/${argv.redux}/store/index.js created`);
+            if (err) {
+              console.log(`Unable to create ${argv.redux} redux store`);
+            } else {
+              console.log(`app/${argv.redux}/store/index.js created`);
+            }
           }
         );
       } else {
         console.log("Check usage: rnhc create --help");
+      }
+    }
+  )
+  .command(
+    "delete [name]",
+    "this command helps you delete your files",
+    (yargs) => {
+      yargs
+        .positional("-c", {
+          alias: "--component",
+          type: "string",
+          describe: "to delete a react-native component",
+        })
+        .positional("-s", {
+          alias: "--screen",
+          type: "string",
+          describe: "to delete a react-native screen",
+        })
+        .positional("-r", {
+          alias: "--redux",
+          type: "string",
+          describe: "to delete basic redux implementation structure)",
+        });
+    },
+    function (argv) {
+      if (argv.component) {
+        fs.unlink(`app/components/${argv.component}.js`, function (err) {
+          if (err) {
+            console.log("component does not exists");
+          } else {
+            console.log(`app/components/${argv.component}.js deleted`);
+          }
+        });
+      } else {
+        console.log("Check usage: rnhc delete --help");
       }
     }
   )
