e8503ce64268ae429ea3a02bfc3a18057807443c
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-11-13 20:24:34 +0100

    🐱‍🚀 delete redux store implementation

--- bin/delete/redux/delete-redux-store.js
+++ bin/delete/redux/delete-redux-store.js
@@ -5,4 +5,12 @@ const fs = require("file-system");
  * @description this function is used to delete redux store implementation that exists.
  * @author [Omar Belghaouti](https://github.com/Omar-Belghaouti)
  */
-exports.deleteReduxStore = () => {};
+exports.deleteReduxStore = () => {
+  const path = "src/redux/";
+  try {
+    fs.rmdirSync(path);
+    console.log(`${path} deleted`);
+  } catch (err) {
+    console.log(`${path} does not exist`);
+  }
+};
