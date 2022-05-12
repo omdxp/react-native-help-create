8120e0b706d4b1b320e65bf6f8c17c6fb8090483
Omar-Belghaouti <bel_omar18@yahoo.com>
2022-05-12 21:18:08 +0100

    combine comand tests

--- tests/combine.spec.js
+++ tests/combine.spec.js
@@ -1,5 +1,106 @@
-const fs = require("file-system");
-
+const { combineComponents, combineScreens } = require("../bin/combine");
+const { createComponent, createScreen } = require("../bin/create");
+const { deleteComponents, deleteScreens } = require("../bin/delete");
 const { sleep, fail, clear } = require("./utils");
 
-describe("combine commands test", () => {});
+const fs = require("file-system");
+
+describe("combine components tests", () => {
+  beforeEach(() => {
+    clear();
+  });
+  afterEach(() => {
+    clear();
+  });
+  console.log = jest.fn();
+  test("should combine components", async () => {
+    try {
+      createComponent("test1", false, true, "", "", false);
+      await sleep(100);
+      createComponent("test2", false, true, "", "", false);
+      await sleep(100);
+      combineComponents(["test1", "test2"], "folder");
+      await sleep(100);
+      expect(fs.existsSync("./src/components/folder/test1/index.tsx")).toBe(
+        true
+      );
+      expect(fs.existsSync("./src/components/folder/test1/styles.ts")).toBe(
+        true
+      );
+      expect(fs.existsSync("./src/components/folder/test2/index.tsx")).toBe(
+        true
+      );
+      expect(fs.existsSync("./src/components/folder/test2/styles.ts")).toBe(
+        true
+      );
+      deleteComponents(["test1", "test2"], "folder");
+      await sleep(100);
+    } catch (err) {
+      fail(err);
+    }
+  });
+
+  test("should not combine not existed components", async () => {
+    try {
+      createComponent("test3", false, true, "", "", false);
+      await sleep(100);
+      combineComponents(["test1", "test2"], "folder");
+      await sleep(100);
+      expect(fs.existsSync("./src/components/folder/test1/index.tsx")).toBe(
+        false
+      );
+      deleteComponents(["test3"], "");
+      await sleep(100);
+    } catch (err) {
+      fail(err);
+    }
+  });
+});
+
+describe("combine screens tests", () => {
+  beforeEach(() => {
+    clear();
+  });
+  afterEach(() => {
+    clear();
+  });
+  console.log = jest.fn();
+  test("should combine screens", async () => {
+    try {
+      createScreen("test1", false, true, "", "", false);
+      await sleep(100);
+      createScreen("test2", false, true, "", "", false);
+      await sleep(100);
+      combineScreens(["test1", "test2"], "folder");
+      await sleep(100);
+      expect(fs.existsSync("./src/screens/folder/test1/index.tsx")).toBe(true);
+      expect(fs.existsSync("./src/screens/folder/test1/styles.ts")).toBe(true);
+      expect(
+        fs.existsSync("./src/screens/folder/test1/functions/index.ts")
+      ).toBe(true);
+      expect(fs.existsSync("./src/screens/folder/test2/index.tsx")).toBe(true);
+      expect(fs.existsSync("./src/screens/folder/test2/styles.ts")).toBe(true);
+      expect(
+        fs.existsSync("./src/screens/folder/test2/functions/index.ts")
+      ).toBe(true);
+      deleteScreens(["test1", "test2"], "folder");
+      await sleep(100);
+    } catch (err) {
+      fail(err);
+    }
+  });
+
+  test("should not combine not existed screens", async () => {
+    try {
+      createScreen("test3", false, true, "", "", false);
+      await sleep(100);
+      combineScreens(["test1", "test2"], "folder");
+      await sleep(100);
+      expect(console.log).lastCalledWith("Check if all of these screens exist");
+      deleteScreens(["test3"], "");
+      await sleep(100);
+    } catch (err) {
+      fail(err);
+    }
+  });
+});
